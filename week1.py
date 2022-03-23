ecbd1d1097f5e3df0b0a70567bf234d90cf0ef91
Akshit Bhardwaj <akshitbhardwaj@Akshits-MacBook-Air.local>
2022-03-23 08:19:30 +0530

    week1commit

---
+++ week1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+x= list(pd.read_excel("week1task.xlsx")['log Y'])
+y = list(pd.read_excel("week1task.xlsx")['log x'])
+plt.figure()
+plt.style.use('seaborn')
+plt.scatter(x,y,marker=".",s=100,edgecolors="black",c="yellow")
+plt.title("Excel sheet to Scatter Plot")
+plt.show()
+gi
